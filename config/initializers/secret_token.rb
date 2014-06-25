d17343d96f87a05b21a28bab989ad5e2d95012f9
tengzhk <tengzhk@cimu.com.cn>
2014-06-25 14:45:29 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestSpine::Application.config.secret_key_base = 'e5a4e0d02c86898e5ebf37b2a118523525025220cfae5fc9dd7424c869184f4298ae837108a42d80b74fc567339b05b6e78ac499ae467dd2822eaf82bb96bd27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestSpine::Application.config.session_store :cookie_store, key: '_testSpine_session'
